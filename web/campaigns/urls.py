12c2846ba96400dfbb0b57a2c3693184bcc798ec
sujana <sharmasujana420@gmail.com>
2026-07-12 08:28:19 +0545

    fixed url routing

--- web/campaigns/urls.py
+++ web/campaigns/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('campaigns/new/', views.new_campaign, name='new_campaign'),
     path('campaigns/<uuid:campaign_id>/', views.campaign_detail, name='campaign_detail'),
+    path('campaigns/preview-message/', views.preview_message, name='preview_message'),
     path('contacts/import/', views.import_contacts, name='import_contacts'),
     path('contacts/template/', views.download_template, name='download_template'),
     path('contacts/', views.contact_list, name='contact_list'),
